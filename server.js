2da51c10d3284984d2a69a5085665b12790f2ed6
saad.shafiq <saad.shafiq@systemsltd.com>
2022-02-04 02:16:44 +0500

    sa

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(`./dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 // Start the app by listening on the default Heroku port
