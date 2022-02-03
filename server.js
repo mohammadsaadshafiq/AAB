02ae492304cfb3822fb2fbc52c6e82a1403ebb15
saad.shafiq <saad.shafiq@systemsltd.com>
2022-02-04 02:57:05 +0500

    a

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
     
-    res.sendFile('index.html',{ root: "../AAB/dist"});
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
