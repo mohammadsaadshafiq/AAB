7179318254f67db4a24645b4869bc344215a9dfd
saad.shafiq <saad.shafiq@systemsltd.com>
2022-02-04 02:31:41 +0500

    s

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('*', (req, res) => {
-    res.sendFile(`./dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile('../AAB/dist/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 // Start the app by listening on the default Heroku port
